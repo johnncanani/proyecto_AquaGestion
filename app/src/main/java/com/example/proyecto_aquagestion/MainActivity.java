84b990256b02da33f6c452ad40b061633b159e86
YersonTineo56 <yeileon1410@gmail.com>
2024-07-07 23:07:28 -0500

    Crear Usuario Local
    se a creado un usuario local para probar la actividad de menu principal

--- app/src/main/java/com/example/proyecto_aquagestion/MainActivity.java
+++ app/src/main/java/com/example/proyecto_aquagestion/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.proyecto_aquagestion;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,15 +16,60 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText txt_usuario, txt_contrasenia;
+    Button btn_ingresar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        txt_usuario = findViewById(R.id.txt_usuario);
+        txt_contrasenia = findViewById(R.id.txt_contrasenia);
+        btn_ingresar = findViewById(R.id.btn_ingresar);
+
+        btn_ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String usuario = txt_usuario.getText().toString();
+                String contrasenia = txt_contrasenia.getText().toString();
+
+                if (usuario.equals("admin") && contrasenia.equals("12345")) {
+                    Toast.makeText(MainActivity.this, "Login exitoso", Toast.LENGTH_SHORT).show();
+                    // Aquí puedes añadir la lógica para redirigir a otra actividad si el login es exitoso
+                } else {
+                    Toast.makeText(MainActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
