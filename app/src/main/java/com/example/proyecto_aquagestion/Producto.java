d5170215896588533021d166de52947f5b9a726c
YersonTineo56 <150179944+YersonTineo56@users.noreply.github.com>
2024-07-19 22:56:43 -0500

    Clase producto java
    Se creo la clase producto donde estara la descripcion del producto

---
+++ app/src/main/java/com/example/proyecto_aquagestion/Producto.java
@@ -0,0 +1,45 @@
+package com.example.proyecto_aquagestion;
+
+public class Producto {
+    private int id;
+    private String name;
+    private String capacity;
+    private String description;
+    private String price;
+    private String imageUri;
+
+    public Producto(int id, String name, String capacity, String description, String price, String imageUri) {
+        this.id = id;
+        this.name = name;
+        this.capacity = capacity;
+        this.description = description;
+        this.price = price;
+        this.imageUri = imageUri;
+    }
+
+    public Producto(String name, String capacity, String description, String price, String imageUri) {
+        this.name = name;
+        this.capacity = capacity;
+        this.description = description;
+        this.price = price;
+        this.imageUri = imageUri;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCapacity() { return capacity; }
+    public void setCapacity(String capacity) { this.capacity = capacity; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getPrice() { return price; }
+    public void setPrice(String price) { this.price = price; }
+
+    public String getImageUri() { return imageUri; }
+    public void setImageUri(String imageUri) { this.imageUri = imageUri; }
+}
